3eefb0ab31b93d176a362c47d5213c4fb43663c7
Armansyah18 <aarmansyah673@gmail.com>
2024-12-05 23:36:14 +0800

    Add files via upload

---
+++ prog8.py
@@ -0,0 +1,13 @@
+# ARITMATIKA (X : + - ** % //)
+
+a = 24 #
+b = 14 #
+
+# penjumlahan 
+hasil = a + b
+print("nilai dari a + b =",hasil)
+
+# perpangkatan
+hasil a ** b
+print("nilai dari a pangkat b = ", hasil)
+(38)
