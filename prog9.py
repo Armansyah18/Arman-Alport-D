ad029247fe2c61cb741cfd09c500b9864373d791
Armansyah18 <aarmansyah673@gmail.com>
2024-12-13 18:43:12 +0700

    Update prog9.py

---
+++ prog9.py
@@ -0,0 +1,17 @@
+
+
+# masukan nilaidalam meter
+# nilai dalam milimeter = 24
+# nilai dalam centimeter = 14
+# nilai dalam kilometer = 664
+
+nilai = int(imput("masukan nilai dalam meter :")
+
+hasil = nilai * 1000
+print("nilai dalam milimeter :", hasil)
+
+
+# suhu calsius (14)
+# suhu dalam fahrenheit
+# suhu dalam kelvin
+# suhu dalam reamur
