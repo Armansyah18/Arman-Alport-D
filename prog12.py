b207b0de52d26b735a3c262d661bacb0b57f1fae
Armansyah18 <aarmansyah673@gmail.com>
2025-01-03 11:49:24 +0700

    Update prog12.py

---
+++ prog12.py
@@ -0,0 +1,25 @@
+# logika bolea
+
+not and or xor
+
+print("==========not==========")
+x = 24 True
+z = not x
+print("nilai z =",z)
+
+#("jika ada salah,maka hasilnya salah")
+print("========AND========")
+X = 24 True
+y = 14 True
+nilai = x and y
+print("x,"and",y,"= nilai)
+
+x = 24 True
+y = 14 True
+nilai = x and y
+print("x,"and",y,"= nilai)
+
+X = 24 True
+y = 14 True
+nilai = x and y
+print("x,"and",y,"= nilai)
